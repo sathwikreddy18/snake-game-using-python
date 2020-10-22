3bfd6e657ba45da76630f1d8fc540905f672360f
sathwikreddy18 <61789539+sathwikreddy18@users.noreply.github.com>
2020-10-22 20:37:44 +0530

    Add files via upload

---
+++ snakegame.py
@@ -0,0 +1,44 @@
+from turtle import *
+from random import randrange
+from freegames import square,vector
+food=vector(0,0)
+snake=[vector(10,0)]
+aim=vector(0,-10)
+def changes(x,y):
+    aim.x=x
+    aim.y=y
+
+def inside(head):
+    return -275<head.x<275 and -275<head.y<275
+def move():
+    head=snake[-1].copy()
+    head.move(aim)
+
+    if not inside(head) or head in snake:
+        square(head.x,head.y,9,'red')
+        update()
+        return
+    snake.append(head)
+    if head==food:
+        print('snake',len(snake))
+        food.x=randrange(-15,15)*10
+        food.y=randrange(-15,15)*10
+    else:
+        snake.pop(0)
+    clear()
+    for body in snake:
+        square(body.x,body.y,9,'green')
+
+    square(food.x,food.y,9,'red')
+    update()
+    ontimer(move,100)
+
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda:changes(10,0),'Right')
+onkey(lambda:changes(-10,0),'Left')
+onkey(lambda:changes(0,10),'Up')
+onkey(lambda:changes(0,-10),'Down')
+move()
+done()
